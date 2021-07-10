7afebd32f49de666c04480ca95e201d04c57beb4
nazeerahmedoffical <nazeerahmedofficial1@gmail.com>
2021-07-10 23:37:23 +0500

    Adds Navbar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import PermanentDrawerRight from './dashboard'
+
+class App extends React.Component{
+
+  render(){
+    return (
+    <PermanentDrawerRight/>
+    )
+  }
+}
+
+export default App;
