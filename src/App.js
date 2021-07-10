3a7ab86a93f597dc2b3aec59b657fdf053225249
jabbar-dev <rashdijabbar@gmail.com>
2021-07-11 00:32:54 +0500

    modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import ClippedDrawer from './dashboard'
 
 class App extends React.Component{
 
   render(){
-    return("Hello World Jabbar")
-  }
+    return(
+    <ClippedDrawer/>
+    )}
 }
 
 export default App;
